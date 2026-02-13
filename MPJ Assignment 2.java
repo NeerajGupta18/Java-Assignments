31400a9faa21ba26172ce7ca8e0707d6336b8ee6
Neeraj Sudesh Gupta <neeraajguptaa@gmail.com>
2026-02-13 22:31:52 +0530

    Create MPJ Assignment 2.java

---
+++ MPJ Assignment 2.java
@@ -0,0 +1,51 @@
+class Employee {
+    double salary;
+
+    Employee(double salary) {
+        this.salary = salary;
+    }
+
+    void displaySalary(String message) {
+        System.out.println(message + salary);
+    }
+}
+
+class FullTimeEmployee extends Employee {
+
+    FullTimeEmployee(double salary) {
+        super(salary);
+    }
+
+    void calculateSalary() {
+        salary = salary + (salary * 0.50); // 50% hike
+    }
+}
+
+class InternEmployee extends Employee {
+
+    InternEmployee(double salary) {
+        super(salary);
+    }
+
+    void calculateSalary() {
+        salary = salary + (salary * 0.25); // 25% hike
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        FullTimeEmployee fte = new FullTimeEmployee(30000);
+        InternEmployee intern = new InternEmployee(10000);
+
+        fte.displaySalary("Full Time Salary Before Hike: ");
+        fte.calculateSalary();
+        fte.displaySalary("Full Time Salary After Hike: ");
+
+        System.out.println();
+
+        intern.displaySalary("Intern Salary Before Hike: ");
+        intern.calculateSalary();
+        intern.displaySalary("Intern Salary After Hike: ");
+    }
+}
